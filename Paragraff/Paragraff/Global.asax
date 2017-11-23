84943a02595396f3008ea46a99a0ddb8c4724c08
Nikola Kolchakov <nikola.kolchakov@gmail.com>
2017-11-23 15:00:38 +0200

    initial commit

---
+++ Paragraff/Paragraff/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Paragraff.MvcApplication" Language="C#" %>
---
+++ Paragraff/Paragraff/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Paragraff.Startup))]
+namespace Paragraff
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
