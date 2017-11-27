fb8bc7619901d713bee80f61bd7be8239a7e77fa
Nikola Kolchakov <nikola.kolchakov@gmail.com>
2017-11-27 14:22:24 +0200

    bootstrap setup

--- Paragraff/Paragraff/App_Start/BundleConfig.cs
+++ Paragraff/Paragraff/App_Start/BundleConfig.cs
@@ -8,6 +8,27 @@ namespace Paragraff
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/matrix-theme-js").Include(
+                 "~/Content/MatrixTheme/js/bootstrap*",
+                 "~/Content/MatrixTheme/js/excanvas*",
+                 "~/Content/MatrixTheme/js/fullcalendar*",
+                 "~/Content/MatrixTheme/js/jquery*",
+                 "~/Content/MatrixTheme/js/masked*",
+                 "~/Content/MatrixTheme/js/matrix*",
+                 "~/Content/MatrixTheme/js/select2*",
+                 "~/Content/MatrixTheme/js/wysihtml*"));
+
+            bundles.Add(new StyleBundle("~/bundles/matrix-theme-css").Include(
+                "~/Content/MatrixTheme/css/bootstrap*",
+                "~/Content/MatrixTheme/css/font-awesome.css",
+                "~/Content/MatrixTheme/css/fullcalendar.css",
+                "~/Content/MatrixTheme/css/colorpicker.css",
+                "~/Content/MatrixTheme/css/datepicker.css",
+                "~/Content/MatrixTheme/css/jquery*",
+                "~/Content/MatrixTheme/css/select2.css",
+                "~/Content/MatrixTheme/css/uniform.css",
+                "~/Content/MatrixTheme/css/matrix-*"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
@@ -20,11 +41,11 @@ namespace Paragraff
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
+                      //"~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      //"~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
