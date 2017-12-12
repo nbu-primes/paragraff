20f55122fbebe5b898ef632af07f86840df5e838
Nikola Kolchakov <nikola.kolchakov@gmail.com>
2017-12-12 16:56:44 +0200

    start login and logout bootstrap

--- Paragraff/Paragraff/Controllers/AccountController.cs
+++ Paragraff/Paragraff/Controllers/AccountController.cs
@@ -393,7 +393,7 @@ namespace Paragraff.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Login", "Account", new {returnUrl = "asdasd" });
         }
 
         //
