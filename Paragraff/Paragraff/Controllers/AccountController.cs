83fc3bae4750aa66e048c25aa5d5966ee69430be
Nikola Kolchakov <nikola.kolchakov@gmail.com>
2017-12-15 13:32:49 +0200

    work with username instaed of email

--- Paragraff/Paragraff/Controllers/AccountController.cs
+++ Paragraff/Paragraff/Controllers/AccountController.cs
@@ -152,7 +152,7 @@ namespace Paragraff.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new User { UserName = model.Email, Email = model.Email };
+                var user = new User { UserName = model.Username, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
